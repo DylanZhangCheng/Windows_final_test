e4d71f5ceb3d15a42e09d615d34c0525d1762852
DylanZhangCheng <1185403419@qq.com>
2018-12-21 18:10:39 +0800

    windows_final_test

---
+++ 软工六班-张成-2016302580311/windows_imageprocess/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace ImageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button7 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.Filter = "JPG文件|*.jpg|PNG文件|*.png|BMP文件|*.bmp";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(525, 85);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(77, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "打开";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(655, 126);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(83, 28);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "左右";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(775, 85);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "逆时针旋转";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(525, 129);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "灰度化";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(525, 205);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(325, 267);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(525, 41);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(75, 28);
+			this.button7.TabIndex = 7;
+			this.button7.Text = "还原";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(679, 87);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 21);
+			this.textBox1.TabIndex = 12;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(614, 92);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(59, 12);
+			this.label1.TabIndex = 13;
+			this.label1.Text = "输入角度:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(614, 134);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(35, 12);
+			this.label3.TabIndex = 14;
+			this.label3.Text = "翻转:";
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(767, 126);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(83, 28);
+			this.button4.TabIndex = 15;
+			this.button4.Text = "上下";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(655, 41);
+			this.trackBar1.Maximum = 5;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(148, 45);
+			this.trackBar1.TabIndex = 16;
+			this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(614, 43);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(35, 12);
+			this.label5.TabIndex = 17;
+			this.label5.Text = "亮度:";
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(596, 165);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(53, 28);
+			this.button8.TabIndex = 19;
+			this.button8.Text = "另存为";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(525, 165);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(42, 28);
+			this.button6.TabIndex = 20;
+			this.button6.Text = "保存";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox2.Location = new System.Drawing.Point(11, 69);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(237, 124);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 21;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.label2.Location = new System.Drawing.Point(522, 7);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(68, 17);
+			this.label2.TabIndex = 22;
+			this.label2.Text = "附加功能：";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.label4.Location = new System.Drawing.Point(12, 12);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(65, 12);
+			this.label4.TabIndex = 23;
+			this.label4.Text = "图片拼接：";
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox3.Location = new System.Drawing.Point(280, 69);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(229, 124);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 24;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox4.Location = new System.Drawing.Point(11, 235);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(498, 237);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox4.TabIndex = 25;
+			this.pictureBox4.TabStop = false;
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(11, 35);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(91, 28);
+			this.button9.TabIndex = 26;
+			this.button9.Text = "打开图片一：";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(280, 35);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(86, 28);
+			this.button10.TabIndex = 27;
+			this.button10.Text = "打开图片二：";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// button11
+			// 
+			this.button11.Location = new System.Drawing.Point(127, 202);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(105, 25);
+			this.button11.TabIndex = 28;
+			this.button11.Text = "图片拼接";
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// button12
+			// 
+			this.button12.Location = new System.Drawing.Point(305, 200);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(99, 28);
+			this.button12.TabIndex = 29;
+			this.button12.Text = "另存为";
+			this.button12.UseVisualStyleBackColor = true;
+			this.button12.Click += new System.EventHandler(this.button12_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(859, 484);
+			this.Controls.Add(this.button12);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "Form1";
+			this.Text = "Windows图像处理";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+	}
+}
+
---
+++ 软工六班-张成-2016302580311/windows_imageprocess/Form1.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+
+namespace ImageProcessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stopwatch sw = new Stopwatch();
+        Bitmap bitmap;
+        Bitmap bar;
+        string path;
+        string name;
+        //原图的宽和高  
+        int w, h;
+		Image image_1;
+		Image image_2;
+		Image image_4;
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+
+				path = openFileDialog1.FileName;
+				name = openFileDialog1.FileNames.ToString();
+
+				FileStream file = new FileStream(path, FileMode.OpenOrCreate);
+
+
+				pictureBox2.Image = Image.FromStream(file);
+				image_1 = Image.FromStream(file);
+
+				bitmap = (Bitmap)pictureBox2.Image;
+				// bitmap = (Bitmap)Image.FromFile(path);
+				w = bitmap.Width;
+				h = bitmap.Height;
+				pictureBox2.Image = bitmap.Clone() as Image;
+				bar = (Bitmap)pictureBox2.Image.Clone();
+
+				file.Close();
+			}
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+
+				path = openFileDialog1.FileName;
+				name = openFileDialog1.FileNames.ToString();
+
+				FileStream file = new FileStream(path, FileMode.OpenOrCreate);
+
+
+				pictureBox3.Image = Image.FromStream(file);
+				image_2 = Image.FromStream(file);
+
+				bitmap = (Bitmap)pictureBox3.Image;
+				// bitmap = (Bitmap)Image.FromFile(path);
+				w = bitmap.Width;
+				h = bitmap.Height;
+				pictureBox3.Image = bitmap.Clone() as Image;
+				bar = (Bitmap)pictureBox3.Image.Clone();
+
+				file.Close();
+			}
+		}
+
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+			List<Image> imageList = new List<Image>();
+			imageList.Add(image_1);
+			imageList.Add(image_2);
+
+			Image final_image = JoinImage(imageList, 0);
+			pictureBox4.Image = final_image;
+			//if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			//{
+
+			//	path = openFileDialog1.FileName;
+			//	name = openFileDialog1.FileNames.ToString();
+
+			//	FileStream file = new FileStream(path, FileMode.OpenOrCreate);
+
+
+			//	pictureBox4.Image = Image.FromStream(file);
+			//	image_4 = Image.FromStream(file);
+
+			//	bitmap = (Bitmap)pictureBox4.Image;
+			//	// bitmap = (Bitmap)Image.FromFile(path);
+			//	w = bitmap.Width;
+			//	h = bitmap.Height;
+			//	pictureBox4.Image = bitmap.Clone() as Image;
+			//	bar = (Bitmap)pictureBox4.Image.Clone();
+
+			//	file.Close();
+			//}
+		}
+
+		private Image JoinImage(List<Image> imageList, int a)
+		{
+			//图片列表
+			if (imageList.Count <= 0)
+				return null;
+			if (a == 0)
+			{
+				//横向拼接
+				int width = 0;
+				//计算总长度
+				foreach (Image i in imageList)
+				{
+					width += i.Width;
+				}
+				//高度不变
+				//int height = imageList.Max(x => x.Height);
+				int height = imageList[1].Height;
+				//构造最终的图片白板
+				Bitmap tableChartImage = new Bitmap(width, height);
+				Graphics graph = Graphics.FromImage(tableChartImage);
+				//初始化这个大图
+				graph.DrawImage(tableChartImage, width, height);
+				//初始化当前宽
+				int currentWidth = 0;
+				foreach (Image i in imageList)
+				{
+					//拼图
+					graph.DrawImage(i, currentWidth, 0);
+					//拼接改图后，当前宽度
+					currentWidth += i.Width;
+				}
+				return tableChartImage;
+			}
+			else if (a == 1)
+			{
+				//纵向拼接
+				int height = 0;
+				//计算总长度
+				foreach (Image i in imageList)
+				{
+					height += i.Height;
+				}
+				//宽度不变
+				//int width = imageList.Max(x => x.Width);
+				int width = imageList[1].Width;
+				//构造最终的图片白板
+				Bitmap tableChartImage = new Bitmap(width, height);
+				Graphics graph = Graphics.FromImage(tableChartImage);
+				//初始化这个大图
+				graph.DrawImage(tableChartImage, width, height);
+				//初始化当前宽
+				int currentHeight = 0;
+				foreach (Image i in imageList)
+				{
+					//拼图
+					graph.DrawImage(i, 0, currentHeight);
+					//拼接改图后，当前宽度
+					currentHeight += i.Height;
+				}
+				return tableChartImage;
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		private void button12_Click(object sender, EventArgs e)
+		{
+			if (pictureBox4.Image == null) { MessageBox.Show("请导入图片文件"); }
+			else
+			{
+				saveFileDialog1.InitialDirectory = "";
+				saveFileDialog1.Filter = "JPEG (*.jpg)|Bitmap (*.bmp)|*.bmp|*.jpg|EMF (*.emf)|*.emf|PNG (*.png)|*.png|GIF (*.gif)|*.gif|TIFF (*.tif)|*.tif";
+				if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+				{
+					string folderP = saveFileDialog1.FileName;
+					Image img = image_4;
+					img.Save(folderP);
+				}
+			}
+		}
+
+		//打开图片（采用输入流打开，防止文件被锁定而导致只能另存为无法保存）
+		private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                path = openFileDialog1.FileName;
+                name = openFileDialog1.FileNames.ToString();
+
+                FileStream file = new FileStream(path, FileMode.OpenOrCreate);
+
+
+                pictureBox1.Image = Image.FromStream(file);
+
+                bitmap = (Bitmap)pictureBox1.Image;
+                // bitmap = (Bitmap)Image.FromFile(path);
+                w = bitmap.Width;
+                h = bitmap.Height;
+                pictureBox1.Image = bitmap.Clone() as Image;
+                bar = (Bitmap)pictureBox1.Image.Clone();
+
+                file.Close();
+            }
+                
+        }
+
+
+
+
+        //保存
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) { MessageBox.Show("请导入图片文件"); }
+            else
+            {
+                Image img = pictureBox1.Image;
+                img.Save(path);
+                MessageBox.Show("保存成功");
+            }
+        }
+
+        //另存为
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) { MessageBox.Show("请导入图片文件"); }
+            else
+            {
+                saveFileDialog1.InitialDirectory = "";
+                saveFileDialog1.Filter = "Bitmap (*.bmp)|*.bmp|JPEG (*.jpg)|*.jpg|EMF (*.emf)|*.emf|PNG (*.png)|*.png|GIF (*.gif)|*.gif|TIFF (*.tif)|*.tif";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string folderP = saveFileDialog1.FileName;
+                    Image img = pictureBox1.Image;
+                    img.Save(folderP);
+                }
+            }
+        }
+
+        //上下翻转
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) { MessageBox.Show("请导入图片文件"); }
+            else {
+                Bitmap a = new Bitmap(pictureBox1.Image);
+                pictureBox1.Image = RevPic1(a) as Image;
+                bar = (Bitmap)pictureBox1.Image.Clone();
+            }
+        }
+
+        //左右翻转
+         private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) { MessageBox.Show("请导入图片文件"); }
+            
+            else
+            {
+                Bitmap c = new Bitmap(pictureBox1.Image);
+                pictureBox1.Image = RevPic2(c) as Image;
+                bar = (Bitmap)pictureBox1.Image.Clone();
+
+            }
+        }
+
+        //任意角度旋转
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == ""&& pictureBox1.Image == null) { MessageBox.Show("请导入图片文件"); }
+
+            else if(textBox1.Text == "" && pictureBox1.Image != null) { MessageBox.Show("请输入旋转角度"); }
+            else
+            {
+                Bitmap a = new Bitmap(pictureBox1.Image);//得到图片框中的图片  
+                pictureBox1.Image = Rotate(Convert.ToInt32(textBox1.Text),a);
+                bar = (Bitmap)pictureBox1.Image.Clone();
+            }
+        }
+
+       //灰度化处理
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) { MessageBox.Show("请导入图片文件"); }
+            else
+            {
+                Bitmap c = new Bitmap(pictureBox1.Image);
+                pictureBox1.Image = GetGrayImage(c) as Image;
+                bar = (Bitmap)pictureBox1.Image.Clone();
+            }
+        }
+
+       //还原
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = bitmap.Clone() as Image;
+        }
+
+        //亮度条
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {          
+            pictureBox1.Image = Brightness(bar) as Image;
+
+        }
+
+        //灰度化函数
+        Bitmap GetGrayImage(Bitmap image)
+        {       
+            Bitmap result = image.Clone() as Bitmap;
+            Color c = new Color();
+            int ret;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    c = result.GetPixel(i, j);
+                    // 计算点i,j的灰度值
+                    ret = (int)(c.R * 0.299 + c.G * 0.587 + c.B * 0.114);
+
+                    result.SetPixel(i, j, Color.FromArgb(ret, ret, ret));
+                }
+            }
+            return result;
+        }
+
+        //旋转函数
+        public Bitmap Rotate(int angle,Bitmap a)
+        {
+            angle = angle % 360;
+
+            //弧度转换  
+            double radian = angle * Math.PI / 180.0;
+            double cos = Math.Cos(radian);
+            double sin = Math.Sin(radian);
+
+            int W = (int)(Math.Max(Math.Abs(w * cos - h * sin), Math.Abs(w * cos + h * sin)));
+            int H = (int)(Math.Max(Math.Abs(w * sin - h * cos), Math.Abs(w * sin + h * cos)));
+
+            //目标位图  
+            Bitmap dsImage = new Bitmap(W, H);
+            Graphics g = Graphics.FromImage(dsImage);
+
+            g.InterpolationMode = InterpolationMode.Bilinear;
+
+            g.SmoothingMode = SmoothingMode.HighQuality;
+
+            //计算偏移量  
+            Point Offset = new Point((W - w) / 2, (H - h) / 2);
+
+            //构造图像显示区域：让图像的中心与窗口的中心点一致  
+            Rectangle rect = new Rectangle(Offset.X, Offset.Y, w, h);
+            Point center = new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+            g.TranslateTransform(center.X, center.Y);
+            g.RotateTransform(360 - angle);
+
+            //恢复图像在水平和垂直方向的平移  
+            g.TranslateTransform(-center.X, -center.Y);
+            g.DrawImage(a, rect);
+
+            //重至绘图的所有变换  
+            g.ResetTransform();
+
+            g.Save();
+            g.Dispose();
+            return dsImage;
+        }
+
+        //左右镜像翻转函数
+        public Bitmap RevPic1(Bitmap mybm)
+        {
+           int width = mybm.Width;
+           int height  = mybm.Height;
+            Bitmap bm = new Bitmap(width, height);//初始化一个记录经过处理后的图片对象
+            int x, y, z;//x,y是循环次数,z是用来记录像素点的x坐标的变化的
+            Color pixel;
+            for (y = height - 1; y >= 0; y--)
+            {
+                for (x = width - 1, z = 0; x >= 0; x--)
+                {
+                    pixel = mybm.GetPixel(x, y);//获取当前像素的值
+                    bm.SetPixel(z++, y, Color.FromArgb(pixel.R, pixel.G, pixel.B));//绘图
+                }
+            }
+            return bm;//返回经过翻转后的图片
+        }
+
+        //上下镜像翻转函数
+        public Bitmap RevPic2(Bitmap mybm)
+        {
+            int width = mybm.Width;
+            int height = mybm.Height;
+            Bitmap bm = new Bitmap(width, height);//初始化一个记录经过处理后的图片对象
+            int x, y, z;//x,y是循环次数,z是用来记录像素点的x坐标的变化的
+            Color pixel;
+            for (x = 0; x < width; x++)
+            {
+                for (y = height - 1, z = 0; y >= 0; y--)
+                {
+                    pixel = mybm.GetPixel(x, y);//获取当前像素的值
+                    bm.SetPixel(x, z++, Color.FromArgb(pixel.R, pixel.G, pixel.B));//绘图
+                }
+            }
+            return bm;//返回经过翻转后的图片
+        }
+
+		
+
+
+
+
+
+		//亮度函数
+		private Bitmap Brightness(Bitmap map)
+        {
+            float grain = trackBar1.Value;
+            Color c;
+            Func<int, int> notOver255 = (x) => { return x > 255 ? 255 : x; };
+            for (int y = 0; y < map.Height; y++)
+            {
+                for (int x = 0; x < map.Width; x++)
+                {
+                    c = map.GetPixel(x, y);
+                    Color brightColor = Color.FromArgb(
+                        notOver255((int)(c.R * grain)),
+                        notOver255((int)(c.G * grain)),
+                        notOver255((int)(c.B * grain)));
+                    map.SetPixel(x, y, brightColor);
+                }
+            }
+            return map;
+        }
+    }
+
+	
+}
